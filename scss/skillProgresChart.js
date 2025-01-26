3503ca85b465dcc3fb9ac5512316e7a4a23c00d7
David Smětala <x213@centrum.cz>
2025-01-26 20:03:54 +0100

    many updates later

---
+++ scss/skillProgresChart.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const skills = document.querySelectorAll(".skill-item");
+  const animateSkill = (skill) => {
+    const circle = skill.querySelector(".progress");
+    const percent = skill.dataset.percent;
+    const percentText = skill.querySelector(".skill-percent");
+    const radius = circle.r.baseVal.value;
+    const circumference = radius * 2 * Math.PI;
+    const offset = circumference - (percent / 100) * circumference;
+    circle.style.strokeDashoffset = offset;
+    let count = 0;
+    const timer = setInterval(() => {
+      if (count >= percent) {
+        clearInterval(timer);
+      } else {
+        count++;
+        percentText.textContent = count + "%";
+      }
+    }, 15);
+  };
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          animateSkill(entry.target);
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+  skills.forEach((skill) => observer.observe(skill));
+});
+
+// Code for the navbar collapse
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".navbar-collapse a");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      // Najdeme instanci collapse a zavřeme
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false,
+      });
+      bsCollapse.hide();
+    });
+  });
+});
